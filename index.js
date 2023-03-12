4706ad83ee7924d9569c023d7622b98df8896fd9
manoti <manotimike@gmail.com>
2023-03-12 05:29:52 +0300

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+const ul = document.getElementById("main");
+
+ul.remove();
+
+const newHeader = document.createElement("h1");
+document.body.appendChild(newHeader);
+
+newHeader.setAttribute("id", "victory");
+
+newHeader.textContent = "Mike is the champion";
